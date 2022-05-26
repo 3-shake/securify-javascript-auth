00d212d0d3322fc9b706b795e6147d26c4e38fb4
ishihaya <ishidahayate49@gmail.com>
2022-05-26 14:29:11 +0900

    デバッグ用のJS認証の場所を用意した

---
+++ index.js
@@ -0,0 +1,27 @@
+(async() => {
+    // エントリポイント
+    const fs = require('fs')
+    try {
+        const requestSourceCode = fs.readFileSync("./test.js", 'utf8')
+        const responseHeaders = await eval(`
+        ${requestSourceCode}
+        (async() => {
+            const headers = await authentication()
+            return headers
+        })()
+        `)
+        // 成功した場合はヘッダを出力します
+        console.log('認証ヘッダーの取得に成功しました。')
+        console.log('ヘッダーの返り値')
+        console.log(responseHeaders)
+        return responseHeaders
+    } catch (e) {
+        // 失敗した場合はエラーメッセージが表示されます
+        console.log('認証ヘッダーの取得に失敗しました。')
+        console.log('エラーメッセージ')
+        console.log(e?.message)
+        console.log('エラー詳細')
+        console.log(e)
+        return
+    }
+})()
---
+++ test.js
@@ -0,0 +1,7 @@
+const authentication = async () => {
+    // NOTE: npmパッケージに関してはpackage.jsonに既にあるパッケージのみ利用できます
+
+    // ここに処理を記述します
+
+    return {"exampleKey": "exampleValue"}
+}
